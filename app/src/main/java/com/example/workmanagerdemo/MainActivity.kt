f713873c9416332030adf465893381719af8a46b
mudasirBhatti <mohammad.mudasir@transdata.biz>
2021-07-05 16:35:05 +0500

    integration

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.workmanagerdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.*
+
 
 class MainActivity : AppCompatActivity() {
+    private var buttonEnque: Button? = null
+    private var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonEnque = findViewById(R.id.buttonEnqueue)
+        textView = findViewById(R.id.textViewStatus)
+
+        //creating constraints
+        val constraints: Constraints = Constraints.Builder()
+            .setRequiresCharging(true) // you can add as many constraints as you want
+            .build()
+
+        // passing data
+        val data: Data = Data.Builder()
+            .putString(MyWorker.TASK_DESC, "The task data passed from MainActivity")
+            .build()
+
+
+        //This is the subclass of our WorkRequest
+        val workRequest: WorkRequest = OneTimeWorkRequestBuilder<MyWorker>()
+            .setInputData(data)
+            .setConstraints(constraints)
+            .build()
+
+        buttonEnque?.setOnClickListener {
+            //Enqueue the work request
+            WorkManager.getInstance(this).enqueue(workRequest)
+        }
+
+        //Listening to the work status
+        WorkManager.getInstance().getWorkInfoByIdLiveData(workRequest.getId())
+            .observe(this, { workInfo -> //Displaying the status into TextView
+                textView?.append(
+                    """
+            ${workInfo.state.name}
+            
+            """.trimIndent()
+                )
+            })
     }
+
 }
+
---
+++ app/src/main/java/com/example/workmanagerdemo/MyWorker.kt
@@ -0,0 +1,51 @@
+package com.example.workmanagerdemo
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+
+class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+/*
+* This method is responsible for doing the work
+* so whatever work that is needed to be performed
+* we will put it here
+ */
+
+    companion object {
+        val TASK_DESC = "task_desc"
+    }
+
+    override fun doWork(): Result {
+
+        //getting the input data
+        val taskDesc = inputData.getString(TASK_DESC)
+
+        displayNotification("WorkManager", taskDesc!!)
+        return Result.success()
+    }
+
+    private fun displayNotification(title: String, task: String) {
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "mudasir",
+                "bhatti",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notification = NotificationCompat.Builder(applicationContext, "mudasir")
+            .setContentTitle(title)
+            .setContentText(task)
+            .setSmallIcon(R.mipmap.ic_launcher)
+        notificationManager.notify(1, notification.build())
+    }
+}
+
+
